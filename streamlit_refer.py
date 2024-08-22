2459f5ddd1cf0685b0eff96eee91c7528cfdcc95
syslians <tonynala97.naver.com>
2024-08-23 02:27:50 +0900

    Change

--- streamlit_refer.py
+++ streamlit_refer.py
@@ -152,7 +152,7 @@ def get_vectorstore(text_chunks):
     return vectordb
 
 def get_conversation_chain(vetorestore,openai_api_key):
-    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name = 'gpt-3.5-turbo',temperature=0)
+    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name = 'gpt-3.5-turbo',temperature=0.2)
     conversation_chain = ConversationalRetrievalChain.from_llm(
             llm=llm, 
             chain_type="stuff", 
